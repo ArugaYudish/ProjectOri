d6e36c46a0b97e7d5dd2efeba11fb666d71b8a5e
repiyann <muhrepiyan26@gmail.com>
2024-06-27 06:51:14 +0700

    feat(dropdown): add dropdown menu for navigation in admin mobile view

--- src/component/common/admin/Sidebar.jsx
+++ src/component/common/admin/Sidebar.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
-import api from '../../../utils/api';
+import api from '../../../utils/api'
 import { Link, useLocation, useNavigate } from 'react-router-dom'
 import logo from '../../../assets/img/OriNeko-Logo.png'
 import '../../../assets/css/navbar.css'
+import { Dropdown, Space } from 'antd'
 
 import { GalleryAdd, LogoutCurve, TransactionMinus, Grid3, ArrangeHorizontal, UserTick } from 'iconsax-react'
 
@@ -47,11 +48,47 @@ const SidebarAdmin = ({ children }) => {
 		}
 	]
 
+	const items = menuItems.map((item, index) => ({
+		label: (
+			<Link
+				to={item.href}
+				className={`flex items-center p-2  rounded-lg  bg-sidebar-sec ${
+					location.pathname === item.href ? 'bg-sidebar' : 'bg-sidebar-sec '
+				}`}
+			>
+				<item.icon
+					className={`${location.pathname === item.href ? 'icon-Wallet ' : ' '}`}
+					variant={location.pathname === item.href ? 'Bold' : 'Regular'}
+				/>
+				<span className={`ms-3 ${location.pathname === item.href ? 'sidebar-color font-bold' : 'text-sidebar '}`}>
+					{item.label}
+				</span>
+			</Link>
+		),
+		key: String(index)
+	}))
+
+	items.push({
+		label: (
+			<button
+				onClick={handleLogout}
+				className="flex items-center p-2 rounded-lg bg-sidebar-sec w-full"
+			>
+				<LogoutCurve
+					size="20"
+					variant="Regular"
+				/>
+				<span className="ms-3 text-sidebar">Logout</span>
+			</button>
+		),
+		key: 'logout'
+	})
+
 	return (
 		<div>
 			<aside
 				id="logo-sidebar"
-				className=" fixed top-0 left-0 z-40 w-64 h-screen  transition-transform -translate-x-full  border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700"
+				className="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full  border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700"
 				aria-label="Sidebar"
 			>
 				<a
@@ -102,6 +139,31 @@ const SidebarAdmin = ({ children }) => {
 					</ul>
 				</div>
 			</aside>
+
+			<div className="block sm:hidden px-6 sm:ml-64">
+				<div className="mt-10">
+					<Dropdown
+						menu={{ items }}
+						trigger={['click']}
+					>
+						<button onClick={(e) => e.preventDefault()}>
+							<Space>
+								<svg
+									className="w-6 h-6 fill-current"
+									viewBox="0 0 24 24"
+									xmlns="http://www.w3.org/2000/svg"
+								>
+									<path
+										fillRule="evenodd"
+										clipRule="evenodd"
+										d="M3 6h18v2H3V6zm0 5h18v2H3v-2zm0 5h18v2H3v-2z"
+									/>
+								</svg>
+							</Space>
+						</button>
+					</Dropdown>
+				</div>
+			</div>
 
 			<div className="p-6 sm:ml-64">
 				<div className="p-4 mt-10">
