bc8b188392b74b66542de9ba793033c7e7c1b445
repiyann <muhrepiyan26@gmail.com>
2024-06-27 10:30:41 +0700

    feat(edit & remove): add fucntion to navigate to edit user and function to remove user into inactive

--- src/pages/admin/user/index.jsx
+++ src/pages/admin/user/index.jsx
@@ -1,122 +1,150 @@
-import React, { useEffect, useState } from 'react';
-import SidebarAdmin from '../../../component/common/admin/Sidebar';
-import '../../../assets/css/user.css';
-import { Table, Button, Spin, message } from 'antd';
-import { AddCircle } from 'iconsax-react';
-import api from '../../../utils/api';
-import { Link, useNavigate } from 'react-router-dom';
+import React, { useEffect, useState } from 'react'
+import SidebarAdmin from '../../../component/common/admin/Sidebar'
+import '../../../assets/css/user.css'
+import { Table, Button, Spin, message } from 'antd'
+import { AddCircle } from 'iconsax-react'
+import api from '../../../utils/api'
+import { Link, useNavigate } from 'react-router-dom'
 
 const Users = () => {
-  const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const navigate = useNavigate()
+	const [data, setData] = useState([])
+	const [loading, setLoading] = useState(true)
+	const [error, setError] = useState(null)
+	const navigate = useNavigate()
 
-  useEffect(() => {
-    const fetchUsers = async () => {
-      try {
-        const response = await api.get('/api/v1/users');
-        if (response.data && response.data.data) {
-          setData(response.data.data.users); // Sesuaikan dengan struktur respons API
-        }
-        setLoading(false);
-      } catch (error) {
-        console.error('Failed to fetch users:', error);
-        message.error('Failed to fetch users');
-        setLoading(false);
-      }
-    };
+	useEffect(() => {
+		const fetchUsers = async () => {
+			try {
+				const response = await api.get('/api/v1/users')
+				if (response.data && response.data.data) {
+					setData(response.data.data.users) // Sesuaikan dengan struktur respons API
+				}
+				setLoading(false)
+			} catch (error) {
+				console.error('Failed to fetch users:', error)
+				message.error('Failed to fetch users')
+				setLoading(false)
+			}
+		}
 
-    fetchUsers();
-  }, []);
+		fetchUsers()
+	}, [])
 
-  const handleButtonClick = record => {
-    navigate(`/asdhakdls/users/edit/${record.id}`);
-  };
+	const handleEdit = (record) => {
+		navigate(`/asdhakdls/users/edit/${record.id}`)
+	}
 
-  const columns = [
-    {
-      title: 'Username',
-      dataIndex: 'name',
-      width: 250,
-    },
-    {
-      title: 'Email',
-      dataIndex: 'email',
-      width: 200,
-    },
-    {
-      title: 'Wallet',
-      dataIndex: 'balance',
-      width: 200,
-    },
-    {
-      title: 'Role',
-      dataIndex: 'role',
-      width: 150,
-    },
-    {
-      title: 'Status',
-      dataIndex: 'status',
-      width: 150,
-      render: status => (status === 1 ? 'Active' : 'Inactive'),
-    },
-    {
-      title: 'Action',
-      dataIndex: 'action',
-      render: (text, record) => (
-        <div className='flex gap-2'>
-          <Button
-            className='bg-orineko-primary text-white border justify-center flex gap-2 items-center rounded-lg text-sm'
-            onClick={() => handleButtonClick(record)}>
-            Edit
-          </Button>
-          <Button
-            className='bg-orineko-danger text-white border justify-center flex gap-2 items-center rounded-lg text-sm'
-            onClick={() => handleButtonClick(record)}>
-            Remove
-          </Button>
-        </div>
-      ),
-    },
-  ];
+	const handleRemove = async (record) => {
+		try {
+			const response = await api.post('/api/v1/users/change-status', {
+				userid: record.id,
+        status: 0
+			})
 
-  return (
-    <SidebarAdmin>
-      <div>
-        <div className='text-3xl py-2 border-b'>User Management</div>
-        <div className='mt-5'>
-          <div className='flex justify-between'>
-            <div className='font-bold py-2'>Users</div>
-            <div>
-              <Link
-                to={'/asdhakdls/users/add'}
-                className='text-white flex gap-2 items-center bg-color-orineko focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700'>
-                <AddCircle size='16' color='#FFFFFF' />
-                Create New
-              </Link>
-            </div>
-          </div>
-          <div className='pt-3'>
-            <div className='card'>
-              {loading ? (
-                <Spin tip='Loading...' />
-              ) : (
-                <Table
-                  className='table-ant'
-                  columns={columns}
-                  dataSource={data}
-                  pagination={{
-                    pageSize: 5,
-                  }}
-                  rowKey={record => record.id} // Asumsikan setiap record memiliki id unik
-                />
-              )}
-            </div>
-          </div>
-        </div>
-      </div>
-    </SidebarAdmin>
-  );
-};
+      console.log('response3', response)
 
-export default Users;
+			if (response.data && response.data.data) {
+				navigate('/asdhakdls/users') // Redirect to the users management page after success
+			} else {
+				setError('Failed to update user')
+			}
+		} catch (error) {
+			setError(error.response?.data?.message || 'Failed to update user')
+		}
+	}
+
+	const columns = [
+		{
+			title: 'Username',
+			dataIndex: 'name',
+			width: 250
+		},
+		{
+			title: 'Email',
+			dataIndex: 'email',
+			width: 200
+		},
+		{
+			title: 'Wallet',
+			dataIndex: 'balance',
+			width: 200
+		},
+		{
+			title: 'Role',
+			dataIndex: 'role',
+			width: 150
+		},
+		{
+			title: 'Status',
+			dataIndex: 'status',
+			width: 150
+		},
+		{
+			title: 'Action',
+			dataIndex: 'action',
+			render: (text, record) => (
+				<div className="flex gap-2">
+					<Button
+						className="bg-orineko-primary text-white border justify-center flex gap-2 items-center rounded-lg text-sm"
+						onClick={() => handleEdit(record)}
+					>
+						Edit
+					</Button>
+					<Button
+						className="bg-orineko-danger text-white border justify-center flex gap-2 items-center rounded-lg text-sm"
+						onClick={() => handleRemove(record)}
+					>
+						Remove
+					</Button>
+				</div>
+			)
+		}
+	]
+
+	return (
+		<SidebarAdmin>
+			<div>
+				<div className="text-3xl py-2 border-b">User Management</div>
+				<div className="mt-5">
+					<div className="flex justify-between">
+						<div className="font-bold py-2">Users</div>
+						<div>
+							<Link
+								to={'/asdhakdls/users/add'}
+								className="text-white flex gap-2 items-center bg-color-orineko focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+							>
+								<AddCircle
+									size="16"
+									color="#FFFFFF"
+								/>
+								Create New
+							</Link>
+						</div>
+					</div>
+					<div className="pt-3">
+						<div className="card">
+							{loading ? (
+								<Spin tip="Loading..." />
+							) : (
+								<>
+									{error && <div className="text-red-500 pb-3">{error}</div>}
+									<Table
+										className="table-ant"
+										columns={columns}
+										dataSource={data}
+										pagination={{
+											pageSize: 5
+										}}
+										rowKey={(record) => record.id} // Asumsikan setiap record memiliki id unik
+									/>
+								</>
+							)}
+						</div>
+					</div>
+				</div>
+			</div>
+		</SidebarAdmin>
+	)
+}
+
+export default Users
