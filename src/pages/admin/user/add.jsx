092882506b7976ea7ea78b056f523360e9a7a117
repiyann <muhrepiyan26@gmail.com>
2024-06-27 10:29:58 +0700

    feat(edit): add edit user page

--- src/pages/admin/user/add.jsx
+++ src/pages/admin/user/add.jsx
@@ -107,7 +107,7 @@ const AddUserForm = () => {
           </div>
           <button
             type='submit'
-            className='text-white bg-blue-500 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-full px-5 py-2.5 text-center'>
+						className="text-white bg-color-orineko hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-gray-300 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700 font-medium rounded-lg text-sm w-full sm:w-full px-5 py-2.5 text-center">
             Add User
           </button>
         </form>
--- src/pages/admin/user/edit.jsx
+++ src/pages/admin/user/edit.jsx
@@ -8,8 +8,8 @@ import '../../../assets/css/auth.css'
 const EditUserForm = () => {
 	const [name, setName] = useState('')
 	const [email, setEmail] = useState('')
-	const [role, setRole] = useState('user') // Default role is 'user'
-  const [status, setStatus] = useState('')
+	const [password, setPassword] = useState('');
+	const [role, setRole] = useState('user'); // Default role is 'user'
 	const [error, setError] = useState(null)
   const navigate = useNavigate()
   const { id } = useParams();
@@ -23,7 +23,6 @@ const EditUserForm = () => {
 					setName(user[0].name)
 					setEmail(user[0].email)
 					setRole(user[0].role)
-          setStatus(user[0].status)
 				}
 			} catch (error) {
 				console.error('Failed to fetch users:', error)
@@ -40,7 +39,11 @@ const EditUserForm = () => {
 		try {
 			const response = await api.post('/api/v1/users/update-user', {
 				id: id,
-				status: status,
+        name: name,
+				email: email,
+				role: role,
+				password: password,
+				password_confirm: password
 			})
 
       console.log(response)
@@ -58,7 +61,7 @@ const EditUserForm = () => {
 	return (
 		<SidebarAdmin>
 			<div className="container mx-auto p-8">
-				<div className="text-2xl font-bold pb-3">Add New User</div>
+				<div className="text-2xl font-bold pb-3">Edit User</div>
 				{error && <div className="text-red-500 pb-3">{error}</div>}
 				<form
 					onSubmit={handleSubmit}
@@ -77,7 +80,6 @@ const EditUserForm = () => {
 							className="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
 							placeholder="Full Name"
 							value={name}
-              disabled
 							onChange={(e) => setName(e.target.value)}
 							required
 						/>
@@ -95,51 +97,45 @@ const EditUserForm = () => {
 							className="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
 							placeholder="Email"
 							value={email}
-              disabled
 							onChange={(e) => setEmail(e.target.value)}
 							required
 						/>
 					</div>
-					<div className="pb-3">
-						<label
-							htmlFor="role"
-							className="block mb-2 text-sm font-medium text-gray-900"
-						>
-							Role
-						</label>
-						<select
-							id="role"
-							className="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-							value={role}
-              disabled
-							onChange={(e) => setRole(e.target.value)}
-							required
-						>
-							<option value="user">User</option>
-							<option value="admin">Admin</option>
-						</select>
-					</div>
-          <div className="pb-3">
-						<label
-							htmlFor="role"
-							className="block mb-2 text-sm font-medium text-gray-900"
-						>
-							Status
-						</label>
-						<select
-							id="status"
-							className="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-							value={status}
-							onChange={(e) => setStatus(e.target.value)}
-							required
-						>
-							<option value="Active">Active</option>
-							<option value="Inactive">Inactive</option>
-						</select>
-					</div>
+					<div className='pb-2'>
+            <label
+              htmlFor='password'
+              className='block mb-2 text-sm font-medium text-gray-900'>
+              Password
+            </label>
+            <input
+              type='password'
+              id='password'
+              className='border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5'
+              placeholder='Password'
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+              required
+            />
+          </div>
+					<div className='pb-3'>
+            <label
+              htmlFor='role'
+              className='block mb-2 text-sm font-medium text-gray-900'>
+              Role
+            </label>
+            <select
+              id='role'
+              className='border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5'
+              value={role}
+              onChange={e => setRole(e.target.value)}
+              required>
+              <option value='user'>User</option>
+              <option value='admin'>Admin</option>
+            </select>
+          </div>
 					<button
 						type="submit"
-						className="text-white bg-blue-500 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-full px-5 py-2.5 text-center"
+						className="text-white bg-color-orineko hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-gray-300 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700 font-medium rounded-lg text-sm w-full sm:w-full px-5 py-2.5 text-center"
 					>
 						Edit User
 					</button>
