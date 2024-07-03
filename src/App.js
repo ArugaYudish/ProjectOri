b0c984f73c3d3badf2a24bb57bab8be88416bbb3
repiyann <muhrepiyan26@gmail.com>
2024-07-03 14:51:34 +0700

    feat(refreshToken): refreshToken only when user is not afk

--- src/App.js
+++ src/App.js
@@ -1,20 +1,49 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Routes from './routes';
 import { refreshToken } from './utils/api';
 
 const App = () => {
+  const [lastActivityTime, setLastActivityTime] = useState(Date.now());
+
   useEffect(() => {
+    const handleUserActivity = () => {
+      setLastActivityTime(Date.now());
+    };
+
+    // Add event listeners to detect user activity
+    window.addEventListener('mousemove', handleUserActivity);
+    window.addEventListener('keydown', handleUserActivity);
+    window.addEventListener('scroll', handleUserActivity);
+    window.addEventListener('click', handleUserActivity);
+
     const interval = setInterval(async () => {
       try {
-        await refreshToken();
+        const currentTime = Date.now();
+        const timeSinceLastActivity = currentTime - lastActivityTime;
+
+        if (timeSinceLastActivity < 2 * 60 * 1000) { // 10 minutes in milliseconds
+          await refreshToken();
+        } else {
+          sessionStorage.removeItem('userId')
+          sessionStorage.removeItem('role')
+          sessionStorage.removeItem('accessToken')
+          sessionStorage.removeItem('userName')
+          sessionStorage.removeItem('Ballance')
+        }
       } catch (error) {
         console.error('Failed to refresh token:', error);
       }
-    }, 60 * 3 * 1000); // 10 minutes in milliseconds
+    }, 60 * 2 * 1000); // 10 minutes in milliseconds
 
-    // Cleanup interval on component unmount
-    return () => clearInterval(interval);
-  }, []);
+    // Cleanup on component unmount
+    return () => {
+      clearInterval(interval);
+      window.removeEventListener('mousemove', handleUserActivity);
+      window.removeEventListener('keydown', handleUserActivity);
+      window.removeEventListener('scroll', handleUserActivity);
+      window.removeEventListener('click', handleUserActivity);
+    };
+  }, [lastActivityTime]);
 
   return (
     <div className='App'>
