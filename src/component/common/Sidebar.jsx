83c0fca879d2c8cbdbd3e0d81a9c6465b7eaa663
repiyann <muhrepiyan26@gmail.com>
2024-07-04 13:10:03 +0700

    feat(modal): add modal for every action, fix some middleware, and fix some style in navbar

--- src/component/common/Sidebar.jsx
+++ src/component/common/Sidebar.jsx
@@ -1,29 +1,38 @@
-import React, { useState } from 'react'
-import { Link, useLocation, useNavigate } from 'react-router-dom'
-import logo from '../../assets/img/OriNeko-Logo.png'
-import { Wallet } from 'iconsax-react'
-import '../../assets/css/navbar.css'
-import api from '../../utils/api'
-import { Dropdown, Space, Modal, message } from 'antd'
-import { BitcoinConvert, MessageAdd1, TableDocument, LogoutCurve, Home } from 'iconsax-react'
+import React, { useState } from "react"
+import { Link, useLocation, useNavigate } from "react-router-dom"
+import logo from "../../assets/img/OriNeko-Logo.png"
+import { Wallet } from "iconsax-react"
+import "../../assets/css/navbar.css"
+import api from "../../utils/api"
+import { Dropdown, Space, Modal, message } from "antd"
+import {
+	BitcoinConvert,
+	MessageAdd1,
+	TableDocument,
+	LogoutCurve,
+	Home
+} from "iconsax-react"
 
 const Sidebar = ({ children }) => {
 	const [isModalOpen, setIsModalOpen] = useState(false)
 	const location = useLocation()
 	const navigate = useNavigate()
+	const isTransaction = sessionStorage.getItem("isTransaction")
+	const name = sessionStorage.getItem("userName")
+	const role = sessionStorage.getItem("role")
 
 	const handleLogout = async () => {
 		try {
-			const response = await api.get('/api/v1/auth/logout')
+			const response = await api.get("/api/v1/auth/logout")
 			if (response.status === 200) {
 				sessionStorage.clear()
 				setIsModalOpen(false)
-				navigate('/')
+				navigate("/")
 			} else {
-				message.error('Logout failed')
+				message.error("Logout failed")
 			}
 		} catch (error) {
-			console.error('Logout failed', error)
+			console.error("Logout failed", error)
 		}
 	}
 
@@ -40,26 +49,45 @@ const Sidebar = ({ children }) => {
 	}
 
 	const menuItems = [
-		{ href: '/', icon: Home, label: 'Home' },
-		{ href: '/wallet', icon: Wallet, label: 'My Wallet' },
-		{ href: '/withdraw', icon: BitcoinConvert, label: 'Withdraw' },
-		{ href: '/refferal', icon: MessageAdd1, label: 'Refferal' },
-		{ href: '/history', icon: TableDocument, label: 'History' }
+		{ href: "/", icon: Home, label: "Home" },
+		{ href: "/wallet", icon: Wallet, label: "My Wallet" },
+		{ href: "/withdraw", icon: BitcoinConvert, label: "Withdraw" },
+		{ href: "/refferal", icon: MessageAdd1, label: "Refferal" },
+		{ href: "/history", icon: TableDocument, label: "History" }
 	]
 
-	const items = menuItems.map((item, index) => ({
+	const filteredItems = menuItems.filter((item) => {
+		if (isTransaction === "true") {
+			return true
+		}
+		return (
+			item.href !== "/refferal" &&
+			item.href !== "/wallet" &&
+			item.href !== "/withdraw"
+		)
+	})
+
+	const items = filteredItems.map((item, index) => ({
 		label: (
 			<Link
 				to={item.href}
 				className={`flex items-center p-2  rounded-lg  bg-sidebar-sec ${
-					location.pathname === item.href ? 'bg-sidebar' : 'bg-sidebar-sec '
+					location.pathname === item.href ? "bg-sidebar" : "bg-sidebar-sec "
 				}`}
 			>
 				<item.icon
-					className={`${location.pathname === item.href ? 'icon-Wallet ' : ' '}`}
-					variant={location.pathname === item.href ? 'Bold' : 'Regular'}
+					className={`${
+						location.pathname === item.href ? "icon-Wallet " : " "
+					}`}
+					variant={location.pathname === item.href ? "Bold" : "Regular"}
 				/>
-				<span className={`ms-3 ${location.pathname === item.href ? 'sidebar-color font-bold' : 'text-sidebar '}`}>
+				<span
+					className={`ms-3 ${
+						location.pathname === item.href
+							? "sidebar-color font-bold"
+							: "text-sidebar "
+					}`}
+				>
 					{item.label}
 				</span>
 			</Link>
@@ -77,26 +105,19 @@ const Sidebar = ({ children }) => {
 				<span className="ms-3 text-sidebar">Logout</span>
 			</button>
 		),
-		key: 'logout'
+		key: "logout"
 	})
 
 	return (
 		<div>
 			<nav class="fixed top-0 z-50 w-full bg-white border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
 				<div class="sticky  mx-auto flex flex-wrap items-center justify-between mx-auto p-4">
-					<a
-						href="/"
-						class="flex items-center space-x-3 rtl:space-x-reverse"
-					>
-						<img
-							src={logo}
-							class="h-8"
-							alt="Flowbite Logo"
-						/>
+					<a href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
+						<img src={logo} class="h-8" alt="Flowbite Logo" />
 					</a>
 					<Dropdown
 						menu={{ items }}
-						trigger={['click']}
+						trigger={["click"]}
 						className="block sm:hidden items-center inline-flex"
 						overlayClassName="w-64"
 					>
@@ -116,10 +137,7 @@ const Sidebar = ({ children }) => {
 							</Space>
 						</button>
 					</Dropdown>
-					<div
-						class="hidden w-full md:block md:w-auto"
-						id="navbar-default"
-					>
+					<div class="hidden w-full md:block md:w-auto" id="navbar-default">
 						<ul class="font-medium flex items-center flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg  md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 ">
 							<li className="block sm:hidden">
 								<a
@@ -155,7 +173,7 @@ const Sidebar = ({ children }) => {
 							</li>
 							<li>
 								<a
-									href="/wallet"
+									href="/history"
 									class=" block py-2 px-3  rounded   md:border-0  md:p-0 dark:text-white  dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
 								>
 									Dashboard
@@ -202,18 +220,16 @@ const Sidebar = ({ children }) => {
 								</a>
 							</li>
 							<li>
-								<div
-									href="#"
-									class="register-button "
-								>
-									Admin
+								<div href="#" class="register-button ">
+									{name && role && (
+										<>
+											{name} - {role}
+										</>
+									)}
 								</div>
 							</li>
 							<li className="block sm:hidden py-5 ">
-								<button
-									onClick={showModal}
-									class=" mt-5 register-button"
-								>
+								<button onClick={showModal} class=" mt-5 register-button">
 									Logout
 								</button>
 							</li>
@@ -229,21 +245,31 @@ const Sidebar = ({ children }) => {
 			>
 				<div class="h-full  px-3 pb-4 overflow-y-auto bg-white dark:bg-gre-800">
 					<ul className="space-y-2 font-medium">
-						{menuItems.map((item) => (
+						{filteredItems.map((item) => (
 							<li key={item.href}>
 								<Link
 									to={item.href}
 									className={`flex items-center p-2  rounded-lg  bg-sidebar-sec ${
-										location.pathname === item.href ? 'bg-sidebar' : 'bg-sidebar-sec '
+										location.pathname === item.href
+											? "bg-sidebar"
+											: "bg-sidebar-sec "
 									}`}
 								>
 									<item.icon
 										size="20"
-										className={`  ${location.pathname === item.href ? 'icon-Wallet ' : ' '}`}
-										variant={location.pathname === item.href ? 'Bold' : 'Regular'}
+										className={`  ${
+											location.pathname === item.href ? "icon-Wallet " : " "
+										}`}
+										variant={
+											location.pathname === item.href ? "Bold" : "Regular"
+										}
 									/>
 									<span
-										className={`ms-3 ${location.pathname === item.href ? 'sidebar-color font-bold' : 'text-sidebar '}`}
+										className={`ms-3 ${
+											location.pathname === item.href
+												? "sidebar-color font-bold"
+												: "text-sidebar "
+										}`}
 									>
 										{item.label}
 									</span>
@@ -255,10 +281,7 @@ const Sidebar = ({ children }) => {
 								onClick={showModal}
 								className="flex items-center p-2 rounded-lg bg-sidebar-sec w-full"
 							>
-								<LogoutCurve
-									size="20"
-									variant="Regular"
-								/>
+								<LogoutCurve size="20" variant="Regular" />
 								<span className="ms-3 text-sidebar">Logout</span>
 							</button>
 						</li>
@@ -271,7 +294,7 @@ const Sidebar = ({ children }) => {
 				open={isModalOpen}
 				onOk={handleOk}
 				onCancel={handleCancel}
-				okButtonProps={{ style: {backgroundColor : '#ca9700'} }}
+				okButtonProps={{ style: { backgroundColor: "#ca9700" } }}
 			>
 				<p>Are you sure you want to logout?</p>
 			</Modal>
--- src/pages/admin/user/index.jsx
+++ src/pages/admin/user/index.jsx
@@ -1,32 +1,49 @@
-import React, { useEffect, useState } from 'react'
-import SidebarAdmin from '../../../component/common/admin/Sidebar'
-import '../../../assets/css/user.css'
-import { Table, Button, Spin, message } from 'antd'
-import { AddCircle } from 'iconsax-react'
-import api from '../../../utils/api'
-import { Link, useNavigate } from 'react-router-dom'
+import React, { useEffect, useState } from "react"
+import SidebarAdmin from "../../../component/common/admin/Sidebar"
+import "../../../assets/css/user.css"
+import { Table, Button, Spin, message, Modal } from "antd"
+import { AddCircle } from "iconsax-react"
+import api from "../../../utils/api"
+import { Link, useNavigate } from "react-router-dom"
 
 const Users = () => {
 	const [data, setData] = useState([])
 	const [loading, setLoading] = useState(true)
 	const [error, setError] = useState(null)
 	const navigate = useNavigate()
+	const [isModalOpen, setIsModalOpen] = useState(false)
+	const [record, setRecord] = useState(null)
+
+	const showModal = (record) => {
+		setRecord(record) // store the record for use in handleOk
+		setIsModalOpen(true)
+	}
+
+	const handleOk = async () => {
+		await handleRemove(record)
+		// setRecord(null)
+		setIsModalOpen(false)
+	}
+
+	const handleCancel = () => {
+		setIsModalOpen(false)
+		setRecord(false)
+	}
 
 	useEffect(() => {
 		const fetchUsers = async () => {
 			try {
-				const response = await api.get('/api/v1/users')
+				const response = await api.get("/api/v1/users")
 				if (response.data && response.data.data) {
 					setData(response.data.data.users) // Sesuaikan dengan struktur respons API
 				}
 				setLoading(false)
 			} catch (error) {
-				console.error('Failed to fetch users:', error)
-				message.error('Failed to fetch users')
+				console.error("Failed to fetch users:", error)
+				message.error("Failed to fetch users")
 				setLoading(false)
 			}
-		}
-
+		}		
 		fetchUsers()
 	}, [])
 
@@ -36,63 +53,61 @@ const Users = () => {
 
 	const handleRemove = async (record) => {
 		try {
-			const response = await api.post('/api/v1/users/change-status', {
+			const response = await api.post("/api/v1/users/change-status", {
 				userid: record.id,
 				status: 0
 			})
 
-			console.log('response3', response)
-
 			if (response.data && response.data.meta.code === 200) {
 				window.location.reload()
 			} else {
-				setError('Failed to update user')
+				setError("Failed to update user")
 			}
 		} catch (error) {
-			setError(error.response?.data?.message || 'Failed to update user')
+			setError(error.response?.data?.message || "Failed to update user")
 		}
 	}
 
 	const columns = [
 		{
-			title: 'Username',
-			dataIndex: 'name',
+			title: "Username",
+			dataIndex: "name",
 			width: 250
 		},
 		{
-			title: 'Email',
-			dataIndex: 'email',
+			title: "Email",
+			dataIndex: "email",
 			width: 200
 		},
 		{
-			title: 'Wallet',
-			dataIndex: 'balance',
+			title: "Wallet",
+			dataIndex: "balance",
 			width: 200
 		},
 		{
-			title: 'Role',
-			dataIndex: 'role',
+			title: "Role",
+			dataIndex: "role",
 			width: 150
 		},
 		{
-			title: 'Status',
-			dataIndex: 'status',
+			title: "Status",
+			dataIndex: "status",
 			width: 150,
 			filters: [
 				{
-					text: 'Active',
-					value: 'Active'
+					text: "Active",
+					value: "Active"
 				},
 				{
-					text: 'Inactive',
-					value: 'Inactive'
+					text: "Inactive",
+					value: "Inactive"
 				}
 			],
-			onFilter: (value, record) => record.status.indexOf(value) === 0,
+			onFilter: (value, record) => record.status.indexOf(value) === 0
 		},
 		{
-			title: 'Action',
-			dataIndex: 'action',
+			title: "Action",
+			dataIndex: "action",
 			render: (text, record) => (
 				<div className="flex gap-2">
 					<Button
@@ -103,7 +118,7 @@ const Users = () => {
 					</Button>
 					<Button
 						className="bg-orineko-danger text-white border justify-center flex gap-2 items-center rounded-lg text-sm"
-						onClick={() => handleRemove(record)}
+						onClick={() => showModal(record)}
 					>
 						Remove
 					</Button>
@@ -113,50 +128,58 @@ const Users = () => {
 	]
 
 	return (
-		<SidebarAdmin>
-			<div>
-				<div className="text-3xl py-2 border-b">User Management</div>
-				<div className="mt-5">
-					<div className="flex justify-between">
-						<div className="font-bold py-2">Users</div>
-						<div>
-							<Link
-								to={'/asdhakdls/users/add'}
-								className="text-white flex gap-2 items-center bg-color-orineko focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
-							>
-								<AddCircle
-									size="16"
-									color="#FFFFFF"
-								/>
-								Create New
-							</Link>
+		<>
+			<Modal
+				title="Notifications"
+				open={isModalOpen}
+				onOk={handleOk}
+				onCancel={handleCancel}
+				okButtonProps={{ style: { backgroundColor: "#ca9700" } }}
+			>
+				<p>Are you sure you want to remove?</p>
+			</Modal>
+			<SidebarAdmin>
+				<div>
+					<div className="text-3xl py-2 border-b">User Management</div>
+					<div className="mt-5">
+						<div className="flex justify-between">
+							<div className="font-bold py-2">Users</div>
+							<div>
+								<Link
+									to={"/asdhakdls/users/add"}
+									className="text-white flex gap-2 items-center bg-color-orineko focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+								>
+									<AddCircle size="16" color="#FFFFFF" />
+									Create New
+								</Link>
+							</div>
 						</div>
-					</div>
-					<div className="pt-3">
-						<div className="card">
-							{loading ? (
-								<Spin tip="Loading..." />
-							) : (
-								<>
-									{error && <div className="text-red-500 pb-3">{error}</div>}
-									<div className="overflow-hidden overflow-x-auto">
-										<Table
-											className="table-ant"
-											columns={columns}
-											dataSource={data}
-											pagination={{
-												pageSize: 5
-											}}
-											rowKey={(record) => record.id} // Asumsikan setiap record memiliki id unik
-										/>
-									</div>
-								</>
-							)}
+						<div className="pt-3">
+							<div className="card">
+								{loading ? (
+									<Spin tip="Loading..." />
+								) : (
+									<>
+										{error && <div className="text-red-500 pb-3">{error}</div>}
+										<div className="overflow-hidden overflow-x-auto">
+											<Table
+												className="table-ant"
+												columns={columns}
+												dataSource={data}
+												pagination={{
+													pageSize: 5
+												}}
+												rowKey={(record) => record.id} // Asumsikan setiap record memiliki id unik
+											/>
+										</div>
+									</>
+								)}
+							</div>
 						</div>
 					</div>
 				</div>
-			</div>
-		</SidebarAdmin>
+			</SidebarAdmin>
+		</>
 	)
 }
 
